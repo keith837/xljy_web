30f0c99902f2303969406e3b7fdfc9026e200239
zhouquan <zhouquan891213@126.com>
2016-02-06 11:40:16 +0800

    提交代码

--- core/config/log4jsConfig.js
+++ core/config/log4jsConfig.js
@@ -4,11 +4,7 @@
 var log4jsConfig  = {
     FILENAME : 'app/logs/access.log',
     //WEBLOGNAME:'app/logs/webacess.log',
-<<<<<<< HEAD
-    LOGLEVEL : "debug",
-=======
     LOGLEVEL : "info",
->>>>>>> remotes/origin/master
     MAXLOGSIZE : 1024 * 1024 * 100 ,
     BACKUPS : 4 ,
     SHOWCONSOLELOG :false
