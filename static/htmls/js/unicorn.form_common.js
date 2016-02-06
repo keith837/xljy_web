0ad2c530ebffae02e4b58c99ef4189e23de50eee
lynn <zhouyoulin12@163.com>
2016-02-06 10:25:06 +0800

    批量导入页面-周友林提交

--- static/htmls/js/unicorn.form_common.js
+++ static/htmls/js/unicorn.form_common.js
@@ -3,7 +3,10 @@
  **/
 $(document).ready(function () {
 
-    $('input[type=checkbox],input[type=radio],input[type=file]').uniform();
+    $('input[type=checkbox],input[type=radio],input[type=file]').uniform({
+            fileDefaultText: '未选择文件',
+            fileBtnText: '浏览'
+    });
 
     $('select').select2();
     $('.colorpicker').colorpicker();
