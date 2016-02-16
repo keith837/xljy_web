708274a01795d7513528a5c35232a8927988468c
Jiao Hui <jeremy_tiny@126.com>
2016-02-17 02:08:53 +0800

    添加基站管理路由

--- app/controllers/deviceController.js
+++ app/controllers/deviceController.js
@@ -4,21 +4,108 @@
 
 var basicController = require("../../core/utils/controller/basicController");
 module.exports = new basicController(__filename).init({
-    devicelist: function (request, response, next) {
+    list: function (request, response, next) {
         var self = this;
-        var start = parseInt(request.body.iDisplayStart);
-        var pagesize = parseInt(request.body.iDisplayLength);
-
-
-        this.model['device'].queryPage(start, pagesize, function (err, allsize, res) {
+        var start = parseInt(request.query.iDisplayStart || 0);
+        var pageSize = parseInt(request.query.iDisplayLength || 10);
+        var queryCondition = {};
+        var schoolId = request.query.schoolId;
+        if (schoolId) {
+            queryCondition.schoolId = parseInt(schoolId);
+        }
+        var classId = request.query.classId;
+        if (classId) {
+            queryCondition.classId = parseInt(classId);
+        }
+        var deviceSign = request.query.deviceSign;
+        if (deviceSign) {
+            queryCondition.deviceSign = deviceSign;
+        }
+        this.model['device'].queryPage(start, pageSize, queryCondition, function (err, totalCount, res) {
             if (err) {
-                next(err);
-                return;
+                return next(err);
             }
-            response.json(self.createPageData(allsize, res));
+            if (totalCount === 0) {
+                return next(new Error("没有查询到手环信息."));
+            } else {
+                response.json({code: "00", data: self.createPageData(totalCount, res)});
+            }
         });
+    },
 
+    addDevice: function (request, response, next) {
+        var param = {};
+        param = parseDevice(request);
+        if (!param.studentId || isNaN(param.studentId)) {
+            return next(new Error("没有输入学生信息."));
+        }
+        this.model['device'].add(param, function (err, insertId) {
+            if (err) {
+                return next(err);
+            } else if (!insertId) {
+                return next(new Error("添加手环记录失败."));
+            } else {
+                response.json({code: "00", msg: "添加手环记录成功."});
+            }
+        });
+    },
 
+    delDevice: function (request, response, next) {
+        var id = parseInt(request.params.id);
+        this.model['device'].del(id, function (err, data) {
+            if (err) {
+                return next(err);
+            } else if (data.affectedRows !== 1) {
+                return next(new Error("删除手环记录失败."));
+            }
+            response.json({code: "00", msg: "删除手环记录成功."});
+        });
+    },
+
+    updateDevice: function (request, response, next) {
+        var param = {};
+        param = parseDevice(request);
+        param.deviceId = parseInt(request.params.id);
+        if (!param.deviceId || isNaN(param.deviceId)) {
+            return next(new Error("没有输入手环ID."));
+        }
+        if (!param.studentId || isNaN(param.studentId)) {
+            return next(new Error("没有输入学生信息"));
+        }
+        this.model['device'].update(param, function (err, data) {
+            if (err) {
+                return next(err);
+            } else if (data.affectedRows !== 1) {
+                return next(new Error("更新手环失败."));
+            } else {
+                response.json({code: "00", msg: "更新手环成功."});
+            }
+        });
+    },
+    detail: function (request, response, next) {
+        var deviceId = request.params.id;
+        this.model['device'].queryDetail(deviceId, function (err, res) {
+            if (err) {
+                return next(err);
+            } else if (res.length === 0) {
+                return next(new Error("无法查询到手环详情[" + deviceId + "]."));
+            }
+            response.json({code: "00", data: res});
+        });
     }
 
 });
+
+
+function parseDevice(request) {
+    var device = {};
+    device.deviceSign = request.body.deviceSign;
+    device.deviceName = request.body.deviceName;
+    device.studentId = parseInt(request.body.studentId);
+    device.state = request.body.state;
+    device.createDate = request.body.createDate;
+    device.doneDate = request.body.doneDate;
+    var userId = request.user.userId;
+    device.oUserId = userId;
+    return device;
+}
--- core/config/routerConfig.js
+++ core/config/routerConfig.js
@@ -102,17 +102,121 @@ var routerConfig = {
                 "description": "动态置顶及下移"
             }
         },
+        station: {
+            "usedModel": ['station/station'],
+            "list": {
+                "url": "/api/stations",
+                "method": "get",
+                "description": "基站列表"
+            },
+            "detail": {
+                "url": "/api/stations/:id",
+                "method": "get",
+                "description": "查询基站详情"
+            },
+            "addStation": {
+                "url": "/api/stations",
+                "method": "post",
+                "description": "添加基站"
+            },
+            "delStation": {
+                "url": "/api/stations/:id",
+                "method": "delete",
+                "description": "删除基站"
+            },
+            "updateStation": {
+                "url": "/api/stations/:id",
+                "method": "put",
+                "description": "更新基站"
+            }
+        },
         device: {
             "usedModel": ['device/device'],
-            "devicelist": {
-                "url": "/api/device/list",
-                "method": "post",
-                "description": "设备列表"
-            },
-            "devicelist2": {
-                "url": "/api/device/list2",
+            "list": {
+                "url": "/api/devices",
                 "method": "get",
-                "description": "设备列表"
+                "description": "手环设备列表"
+            },
+            "detail": {
+                "url": "/api/devices/:id",
+                "method": "get",
+                "description": "查询手环详情"
+            },
+            "addDevice": {
+                "url": "/api/devices",
+                "method": "post",
+                "description": "添加手环"
+            },
+            "delDevice": {
+                "url": "/api/devices/:id",
+                "method": "delete",
+                "description": "删除手环"
+            },
+            "updateDevice": {
+                "url": "/api/devices/:id",
+                "method": "put",
+                "description": "更新手环"
+            }
+        },
+        notice: {
+            "usedModel": ['notice/notice'],
+            "list": {
+                "url": "/api/notices",
+                "method": "get",
+                "description": "查询通知列表"
+            },
+            "publish": {
+                "url": "/api/notices",
+                "method": "post",
+                "description": "发布通知"
+            },
+            "del": {
+                "url": "/api/notices/:id",
+                "method": "delete",
+                "description": "删除通知"
+            },
+            "details": {
+                "url": "/api/notices/:id",
+                "method": "get",
+                "description": "查看通知详情"
+            },
+            "edit": {
+                "url": "/api/notices/:id",
+                "method": "put",
+                "description": "编辑通知"
+            }
+        },
+        photos: {
+            "usedModel": ["photos/photos"],
+            "publish": {
+                "url": "/api/photos",
+                "method": "post",
+                "description": "发布相册"
+            },
+            "delete": {
+                "url": "/api/photos/:id",
+                "method": "delete",
+                "description": "删除相册"
+            },
+            "like": {
+                "url": "/api/photos/like/:id",
+                "method": "put",
+                "description": "相册点赞"
+            },
+            "comment": {
+                "url": "/api/photos/comment/:id",
+                "method": "put",
+                "description": "相册评论"
+            },
+            "list": {
+                "url": "/api/photos",
+                "method": "get",
+                "description": "查看相册"
+            },
+            "edit": {
+                "url": "/api/photos/:id",
+                "method": "put",
+                "description": "编辑相册"
             }
         },
         app: {
