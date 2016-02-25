2a7db7d5b6753a25e73e0040b50d7a459153681f
zhouquan <zhouquan891213@126.com>
2016-02-25 22:34:05 +0800

    提交代码

--- app/dao/school/class.js
+++ app/dao/school/class.js
@@ -4,3 +4,7 @@ var mysqlUtil = require("../../../core/utils/pool/mysql/mysqlPool");
 Class.listByTeacherId = function(teacherId, callback){
     mysqlUtil.query("select * from XL_CLASS where tUserId = ?", [teacherId], callback);
 }
+
+Class.findOne = function(classId, callback){
+    mysqlUtil.queryOne("select * from XL_CLASS where classId = ?", [classId], callback);
+}
