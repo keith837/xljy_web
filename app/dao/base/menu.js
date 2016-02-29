6b3efe4824385cd9cf59709273cd82c5a64e5149
zhouquan <zhouquan891213@126.com>
2016-02-29 23:11:50 +0800

    提交代码

--- app/dao/base/menu.js
+++ app/dao/base/menu.js
@@ -7,5 +7,5 @@ Menu.listAll = function(callback){
 }
 
 Menu.listByRoleId = function(roleId, callback){
-    mysqlUtil.query("select A.* from XL_MENU A,XL_ROLE_MENU_REL B WHERE A.menuId=B.menuId A.state=1 and B.state=1 and B.roleId=?", [roleId], callback);
+    mysqlUtil.query("select A.* from XL_MENU A,XL_ROLE_MENU_REL B WHERE A.menuId=B.menuId and A.state=1 and B.state=1 and B.roleId=?", [roleId], callback);
 }
