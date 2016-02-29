ddf9ae0a1fab433b09d2d7c38ef88bbb126eac27
zhouquan <zhouquan891213@126.com>
2016-03-01 00:06:55 +0800

    提交代码

---
+++ app/controllers/brandController.js
@@ -0,0 +1,126 @@
+var basicController = require("../../core/utils/controller/basicController");
+module.exports = new basicController(__filename).init({
+
+    add : function(req, res, next){
+        var self = this;
+        var brandName = req.body.brandName;
+        var bUserId = req.body.bUserId;
+        var brandUrl = req.body.brandUrl;
+        var remark = req.body.remark;
+        var oUserId = req.user.userId;
+        if(!brandName){
+            return next("品牌名称不能为空");
+        }
+        if(!bUserId){
+            return next("集团园长编号不能为空");
+        }
+        self.model["brand"].save([brandName,bUserId,brandUrl,oUserId,remark], function(err, data){
+            if(err){
+                return next(err);
+            }
+            res.json({
+                code : "00",
+                msg : "品牌添加成功"
+            });
+        });
+    },
+
+    modify : function(req, res, next){
+        var self = this;
+        var brandId = req.params.brandId;
+        var brandName = req.body.brandName;
+        var bUserId = req.body.bUserId;
+        var brandUrl = req.body.brandUrl;
+        var remark = req.body.remark;
+        var oUserId = req.user.userId;
+        if(!brandId || brandId <= 0){
+            return next("品牌编号不能为空");
+        }
+        var obj = new Object();
+        if(brandName){
+            obj.brandName = brandName;
+        }
+        if(brandUrl){
+            obj.brandUrl = brandUrl;
+        }
+        if(bUserId){
+            obj.bUserId = bUserId;
+        }
+        if(remark){
+            obj.remark = remark;
+        }
+        obj.doneDate = new Date();
+        obj.oUserId = oUserId;
+        self.model["brand"].update(obj, brandId, function(err, data){
+            if(err){
+                return next(err);
+            }else if(data.affectedRows != 1){
+                return next(new Error("品牌修改失败"));
+            }
+            res.json({
+                code : "00",
+                msg : "品牌修改成功"
+            });
+        });
+    },
+
+    del : function(req, res, next){
+        var self = this;
+        var brandId = req.params.brandId;
+        self.model['school'].countSchoolByBrandId(brandId, function(err, schools){
+            if(err){
+                return next(err);
+            }
+            if(schools && schools.tatal > 0){
+                return next(new Error("该品牌已关联学校，不允许删除"));
+            }
+            self.model['brand'].del(brandId, function(err, data){
+                if(err){
+                    return next(err);
+                }else if(data.affectedRows != 1){
+                    return next(new Error("需删除的品牌信息不存在"));
+                }
+                res.json({
+                    code : "00",
+                    msg : "品牌删除成功"
+                });
+            });
+        });
+    },
+
+    show : function(req, res, next){
+        var self = this;
+        var brandId = req.params.brandId;
+        self.model['brand'].findByBrandId(brandId, function(err, brand){
+            if(err){
+                return next(err);
+            }
+            res.josn({
+                code : "00",
+                data : brand
+            });
+        });
+    },
+
+    list : function(req, res, next){
+        var self = this;
+        var start = parseInt(req.query.iDisplayStart || this.webConfig.iDisplayStart);
+        var pageSize = parseInt(req.query.iDisplayLength || this.webConfig.iDisplayLength);
+        var obj = new Object;
+        var brandName = req.query.brandName;
+        if(brandName){
+            obj.brandName = brandName;
+        }
+        var bUserId = req.query.bUserId;
+        if(bUserId){
+            obj.bUserId = parseInt(bUserId);
+        }
+        self.model['brand'].listByPage(obj, start, pageSize, function(err, total, schools){
+            if(err){
+                return next(err);
+            }
+            res.json(self.createPageData("00", total, schools));
+        });
+    }
+
+});
---
+++ app/dao/school/brand.js
@@ -0,0 +1,114 @@
+var mysqlUtil = require("../../../core/utils/pool/mysql/mysqlPool");
+
+var Brand = module.exports;
+
+Brand.findByBrandId = function(brandId, callback){
+    var sql = "select A.*,B.custName,B.userName,B.nickName from XL_BRAND A, XL_USER B where A.bUserId=B.userId where";
+    sql += " A.state = 1 and brandId = ?";
+    mysqlUtil.queryOne(sql, [brandId], callback);
+}
+
+/**
+ * 查询品牌数量
+ * @param obj 查询条件
+ * @param callback
+ */
+Brand.queryNum = function(obj, callback){
+    var whereSql = " A.state=1 ";
+    var args = new Array();
+    if(obj){
+        for(var key in obj){
+            whereSql += " and A." + key + "=?";
+            args.push(schoolObj[key]);
+        }
+    }
+    var countSql = "select count(*) as total from XL_BRAND A, XL_USER B where A.bUserId=B.userId where " + whereSql;
+    mysqlUtil.queryOne(countSql, args, callback);
+}
+
+/**
+ * 分页查询品牌信息
+ * @param obj 查询条件
+ * @param start 起始条数
+ * @param pageSize 每页条数
+ * @param callback
+ */
+Brand.queryPage = function(obj, start, pageSize, callback){
+    var whereSql = " A.state=1 ";
+    var args = new Array();
+    if(obj){
+        for(var key in obj){
+            whereSql += " and A." + key + "=?";
+            args.push(obj[key]);
+        }
+    }
+    var querySql = "select A.*,B.custName,B.userName,B.nickName from XL_BRAND A, XL_USER B where A.bUserId=B.userId where " + whereSql;
+    querySql += " limit ?,?";
+    args.push(start);
+    args.push(pageSize);
+    mysqlUtil.query(querySql, args, callback);
+}
+
+/**
+ * 查询品牌数量及信息
+ * @param obj 查询条件
+ * @param start 起始条数
+ * @param pageSize 每页条数
+ * @param callback
+ */
+Brand.listByPage = function(obj, start, pageSize, callback){
+    School.queryNum(obj, function(err, data){
+        if(err){
+            return callback(err);
+        }
+        var total = 0;
+        if(data){
+            total = data.total;
+        }
+        School.queryPage(obj, start, pageSize, function(err, users){
+            if(err){
+                return callback(err);
+            }
+            return callback(err, total, users);
+        });
+    });
+}
+
+/**
+ * 修改学校
+ * @param obj 修改内容
+ * @param schoolId 学校编号
+ * @param callback
+ */
+Brand.update = function(obj, brandId, callback){
+    var sql = "update XL_BRAND set ";
+    var args = new Array();
+    for(var key in obj){
+        sql += key + "=?,";
+        args.push(obj[key]);
+    }
+    sql = sql.substr(0, sql.length - 1);
+    sql += " where brandId=?";
+    args.push(brandId);
+    mysqlUtil.query(sql, args, callback);
+}
+
+/**
+ * 删除品牌信息
+ * @param schoolId 品牌编号
+ * @param callback
+ */
+Brand.del = function(brandId, callback){
+    mysqlUtil.query("update XL_BRAND set state=0 where brandId=?", [brandId], callback);
+}
+
+/**
+ * 保存品牌信息
+ * @param args 参数
+ * @param callback
+ */
+Brand.save = function(args, callback){
+    var sql = "insert into XL_BRAND(brandName,bUserId,brandUrl,state,createDate,doneDate,oUserId,remark";
+    sql += ") values (?,?,?,1,now(),now(),?,?)";
+    mysqlUtil.query(sql, args, callback);
+}
