5e59b97b4f12f049fd32c2ad0207831616974b2d
zhouquan <zhouquan891213@126.com>
2016-03-09 21:35:03 +0800

    提交代码

--- app/controllers/studentController.js
+++ app/controllers/studentController.js
@@ -282,7 +282,7 @@ module.exports = new basicController(__filename).init({
         }
         var studentName = req.query.studentName;
         if(studentName){
-            obj.studentName = studentName;
+            obj.studentName = "%" + studentName + "%";
         }
         self.model["student"].listByPage(obj, schoolIds, start, pageSize, function(err, total, students){
             if(err){
