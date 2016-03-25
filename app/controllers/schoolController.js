448bc3fac1d3d8f8599225418a26af634f1e04f8
Jiao Hui <jeremy_tiny@126.com>
2016-03-25 22:01:21 +0800

    fix bug

--- app/controllers/schoolController.js
+++ app/controllers/schoolController.js
@@ -49,14 +49,14 @@ module.exports = new basicController(__filename).init({
                 log.error(err);
             }
             log.info("删除设备[" + installationId + "]云端token成功，objectId=" + objectId);
-        });
 
-        pushCore.regDevice(user.deviceType, user.installationId, ["school_" + user.schools[0].schoolId], function (err, objectId) {
-            if (err) {
-                log.error("注册设备[" + installationId + "]出错");
-                log.error(err);
-            }
-            log.info("注册设备[" + installationId + "]成功，objectId=" + objectId);
+            pushCore.regDevice(user.deviceType, user.installationId, ["school_" + user.schools[0].schoolId], function (err, objectId) {
+                if (err) {
+                    log.error("注册设备[" + installationId + "]出错");
+                    log.error(err);
+                }
+                log.info("注册设备[" + installationId + "]成功，objectId=" + objectId);
+            });
         });
 
         res.json({
--- app/controllers/studentController.js
+++ app/controllers/studentController.js
@@ -42,17 +42,18 @@ module.exports = new basicController(__filename).init({
                             log.error(err);
                         }
                         log.info("删除设备[" + installationId + "]云端token成功，objectId=" + objectId);
-                    });
 
-                    var channels = [];
-                    channels.push("school_" + user.schools[0].schoolId + "_parent");
-                    channels.push("class_" + user.class.classId);
-                    pushCore.regDevice(user.deviceType, user.installationId, channels, function (err, objectId) {
-                        if (err) {
-                            log.error("注册设备[" + installationId + "]出错");
-                            log.error(err);
-                        }
-                        log.info("注册设备[" + installationId + "]成功，objectId=" + objectId);
+
+                        var channels = [];
+                        channels.push("school_" + user.schools[0].schoolId + "_parent");
+                        channels.push("class_" + user.class.classId);
+                        pushCore.regDevice(user.deviceType, user.installationId, channels, function (err, objectId) {
+                            if (err) {
+                                log.error("注册设备[" + installationId + "]出错");
+                                log.error(err);
+                            }
+                            log.info("注册设备[" + installationId + "]成功，objectId=" + objectId);
+                        });
                     });
 
                     res.json({
